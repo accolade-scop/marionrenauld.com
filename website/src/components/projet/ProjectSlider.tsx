95e936f6e80ba847663f43973e5784101a3433d5
michaelgrosio <michael.grosio@outlook.fr>
2024-03-03 19:50:12 +0100

    add image action

--- website/src/components/projet/ProjectSlider.tsx
+++ website/src/components/projet/ProjectSlider.tsx
@@ -1,43 +1,110 @@
 'use client';
 import { ACMSConfig } from '@/utils/tool';
-import '@splidejs/react-splide/css';
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import { Options } from '@splidejs/splide';
+import React, { PropsWithChildren, useCallback, useEffect, useState } from 'react'
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay'
+import '../../../src/app/assets/globals.sass';
+import { EmblaCarouselType } from 'embla-carousel'
 
 type ProjectType = ACMSConfig<'action'> | ACMSConfig<'objet'>;
 
 export default function ProjectSlider(params: { project: ProjectType }) {
     const {project} = params;
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay({stopOnInteraction: false})]);
+
+    const { selectedIndex, onDotButtonClick } = useImageSelection(emblaApi);
 
     // pas de projet ou pas d'images
     if (!project || !project.image?.length) {
         return '';
     }
 
-    const options: Options = {
-        width: '100%',
-        arrows: false,
-        gap: 10,
-        keyboard: 'global',
-        autoplay: true,
-        drag: true,
-        type: 'loop',
-        perPage: 2,
-        breakpoints: {
-            1000: {
-                perPage: 1
-            }
-        }
-    };
+    return (
+        <div className="embla">
+            <div className="embla__viewport" ref={emblaRef}>
+                <div className="embla__container">
+                    {project.image.map((image, key) =>
+                        <div key={key} className="embla__slide">
+                            <img src={image.src} alt={image.title}/>
+                        </div>
+                    )}
+                </div>
+            </div>
 
+            <div className="embla__dots">
+                {project.image.map((_, index) => (
+                    <ImageSelection
+                        onClick={() => onDotButtonClick(index)}
+                        key={index}
+                        className={'embla__dot'.concat(
+                            index === selectedIndex ? ' embla__dot--selected' : ''
+                        )}
+                    />
+                ))}
+            </div>
+        </div>
+
+    );
+}
+
+type UseImageSelectionType = {
+    selectedIndex: number
+    scrollSnaps: number[]
+    onDotButtonClick: (index: number) => void
+}
+
+export const useImageSelection = (
+    emblaApi: EmblaCarouselType | undefined
+): UseImageSelectionType => {
+    const [selectedIndex, setSelectedIndex] = useState(0)
+    const [scrollSnaps, setScrollSnaps] = useState<number[]>([])
+
+    const onDotButtonClick = useCallback(
+        (index: number) => {
+            if (!emblaApi) return
+            emblaApi.scrollTo(index)
+        },
+        [emblaApi]
+    )
+
+    const onInit = useCallback((emblaApi: EmblaCarouselType) => {
+        setScrollSnaps(emblaApi.scrollSnapList())
+    }, [])
+
+    const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+        setSelectedIndex(emblaApi.selectedScrollSnap())
+    }, [])
+
+    useEffect(() => {
+        if (!emblaApi) return
+
+        onInit(emblaApi)
+        onSelect(emblaApi)
+        emblaApi.on('reInit', onInit)
+        emblaApi.on('reInit', onSelect)
+        emblaApi.on('select', onSelect)
+    }, [emblaApi, onInit, onSelect])
+
+    return {
+        selectedIndex,
+        scrollSnaps,
+        onDotButtonClick
+    }
+}
+
+type PropType = PropsWithChildren<
+    React.DetailedHTMLProps<
+        React.ButtonHTMLAttributes<HTMLButtonElement>,
+        HTMLButtonElement
+    >
+>
+
+export const ImageSelection: React.FC<PropType> = (props) => {
+    const { children, ...restProps } = props
 
     return (
-        <Splide aria-label="Images" options={options}>
-            {project.image.map((image, key) =>
-                <SplideSlide key={key} style={{textAlign: 'center'}}>
-                    <img src={image.src} alt={image.title}/>
-                </SplideSlide>
-            )}
-        </Splide>
-    );
+        <button type="button" {...restProps}>
+            {children}
+        </button>
+    )
 }
