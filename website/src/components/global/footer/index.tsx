9a79d120fefb8384e61543fb45a51e97e81e1c91
michaelgrosio <michael.grosio@outlook.fr>
2025-03-26 08:45:14 +0100

    Ajout des cv

--- website/src/components/global/footer/index.tsx
+++ website/src/components/global/footer/index.tsx
@@ -2,17 +2,16 @@ import Link from "next/link";
 import { ACMS } from '@/utils/tool';
 
 export default async function Footer() {
-    const fileList= await ACMS.getList('cv');
+    const fileList = await ACMS.getList('cv');
     const selected = fileList.sort((a, b) => {
         return new Date(b.lastupdate || '') > new Date(a.lastupdate || '') ? 1 : -1
     })[0];
-    const url = selected.fichier?.src || '';
 
     return (<footer>
             <section>
                 <Link target="_blank"
                       href="http://poincare.univ-lorraine.fr/fr/membre-associe/marion-renauld">philosophie</Link>
-                <Link target="_blank" href={url}>
+                <Link target="_blank" href={selected.fichier?.src || ''}>
                     <img src="/img/cv.svg" className="picto" alt="Lien vers curriculum vitae"/></Link>
                 <Link target="_blank" href="https://www.instagram.com/marionrenauld/">
                     <img src="/img/photo.svg" className="picto" alt="Lien Instagram"/></Link>
